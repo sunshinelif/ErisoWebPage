adbf0b6a5c89de3d4982d21e4908e5b7a1922db7
Boniface C Mwanza <cbmkboniface@gmail.com>
2023-12-20 12:48:48 +0100

    render static

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
-// server.js
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+// const express = require('express');
+// const path = require('path');
 
-const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, 'index.html');
+// const app = express();
 
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-            return;
-        }
+// // Serve static files from the 'public' directory
+// app.use(express.static(path.join(__dirname, 'public')));
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-    });
-});
+// const port = process.env.PORT || 4000;
 
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
